8a0d240508c08106510bdc68095d7191a8325672
Daniel <byoung.mokk@gmail.com>
2023-06-13 23:28:09 +0900

    Feat: 데이터베이스 액세스

---
+++ src/main/java/com/example/sburrestdemo/CoffeeRepository.java
@@ -0,0 +1,5 @@
+package com.example.sburrestdemo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CoffeeRepository extends CrudRepository<Coffee, String> {;;}
--- src/main/java/com/example/sburrestdemo/RestApiDemoController.java
+++ src/main/java/com/example/sburrestdemo/RestApiDemoController.java
@@ -4,71 +4,57 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 public class RestApiDemoController {
 
-    private List<Coffee> coffees = new ArrayList<>();
+    private final CoffeeRepository coffeeRepository;
 
-    public RestApiDemoController() {
-        coffees.addAll(List.of(
+    public RestApiDemoController(CoffeeRepository coffeeRepository) {
+        this.coffeeRepository = coffeeRepository;
+
+        coffeeRepository.saveAll(List.of(
                 new Coffee("아메리카노"),
                 new Coffee("카페라떼"),
                 new Coffee("바닐라라떼"),
                 new Coffee("카푸치노"),
-                new Coffee("카라멜마끼야토")
-        ));
+                new Coffee("카라멜마끼야토")));
     }
 
     @GetMapping("/coffees")
     Iterable<Coffee> getCoffees() {
-        return coffees;
+//        return coffees;
+        return coffeeRepository.findAll();
     }
 
     @GetMapping("/coffees/{id}")
     Optional<Coffee> getCoffeeById(@PathVariable String id) {
 
-        for (Coffee menu : coffees) {
-            if (menu.getId().equals(id)) {
-                return Optional.of(menu);
-            }
-        }
-        return Optional.empty();
+        return coffeeRepository.findById(id);
     }
 
     // 상태코드 사용 권장
     @PostMapping("/coffees")
     Coffee postCoffee(@RequestBody Coffee coffee) {
 
-        coffees.add(coffee);
-        return coffee;
+        return coffeeRepository.save(coffee);
     }
 
     // 상태코드 사용 필수
     @PutMapping("/coffees/{id}")
     ResponseEntity<Coffee> putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
-        int coffeeIndex = -1;
 
-        for (Coffee menu : coffees) {
-            if (menu.getId().equals(id)) {
-                coffeeIndex = coffees.indexOf(menu);
-                coffees.set(coffeeIndex, coffee);
-            }
-        }
-
-//        return coffeeIndex == -1 ? postCoffee(coffee) : coffee;
-        return coffeeIndex == -1 ?
-                new ResponseEntity<>(postCoffee(coffee), HttpStatus.CREATED) :
-                new ResponseEntity<>(coffee, HttpStatus.OK);
+        return (!coffeeRepository.existsById(id)) ?
+                new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.CREATED)
+                : new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.OK);
     }
 
     // 상태코드 사용 권장
     @DeleteMapping("/coffees/{id}")
     void deleteCoffee(@PathVariable String id) {
-        coffees.removeIf(menu -> menu.getId().equals(id));
+        coffeeRepository.deleteById(id);
     }
 
 } // end

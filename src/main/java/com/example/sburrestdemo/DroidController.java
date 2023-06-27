bd893246d2a007c5e75ac53e7baf5fcaefef28ef
Daniel <byoung.mokk@gmail.com>
2023-06-27 23:12:04 +0900

    Feat: actuator/loggers 사용해 로깅수준 변경해보기

--- src/main/java/com/example/sburrestdemo/DroidController.java
+++ src/main/java/com/example/sburrestdemo/DroidController.java
@@ -1,9 +1,10 @@
 package com.example.sburrestdemo;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/droid")
